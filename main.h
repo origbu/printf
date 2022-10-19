81c9d0c3bf5203165af36a6c823a59361e49e576
origbu <samuelbrownadasa@gmail.com>
2022-10-19 10:16:28 -0700

    format specifiers

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef _HEADER_FILE_
+#define _HEADER_FILE_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
