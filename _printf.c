aa8104dadf523cce9bce3e37c02f61ff9f666d6c
origbu <samuelbrownadasa@gmail.com>
2022-10-19 19:34:32 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,27 +1,58 @@
+#include <stdarg.h>
 #include "main.h"
-/**
- * _printf-a function that produces output according to a format
- * @format:character string
- *
- * Return:number of characters printed
- */
-int _printf(const char *format, ...)
-{
-	int size;
-	va_list args;
+#include <stdio.h>
 
-	if (format == NULL)
-		return (-1);
+int _printf(const char *format, ...){
+  int count = 0, i;
+  
+  va_list data;
+  va_start(data, format);
 
-	size = _strlen(format);
-	if (size <= 0)
-		return (0);
+  /* _printf("%s", 'Hello') */
+  
+  for (i = 0; format[i] != '\0'; ){
+    
+    /* count the number of characters */
+    /* print to the screen each character counted */
+    if (format[i] != '%'){
+      count += _putchar(format[i]);
+      i++;
+    }
+    else if (format[i] == '%' && format[i+1] !=' '){
+      switch (format[i + 1]){
+        case 'c':
+            /* print the character from the va_arguments */
+            count += _putchar(va_arg(data, int));
+            break;
+        case 's':
+            count += print_string(va_arg(data, char *));
+            break;
+        case '%':
+            /* print the character from the va_arguments */
+            count += _putchar('%');
+            break;
+        case 'd':
+            count += print_decimal(va_arg(data, int));
+            break;
+        case 'i':
+            count += print_decimal(va_arg(data, int));
+            break;
+        case 'b':
+            count += print_binary(va_arg(data, int));
+            break;
+        case 'u':
+            count += print_unsigned(va_arg(data, unsigned int));
+            break;
+        default:
+            break;
+      }
+      
+      i += 2;
+    }
+    
+    
+  }
+  
 
-	va_start(args, format);
-	size = handler(format, args);
-
-	_putchar(-1);
-	va_end(args);
-
-	return (size);
+  return (count);
 }
